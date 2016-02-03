2cbf874c5120d09fb8179929efd498b44cf9925f
Samuel Loup <loup@epsitec.ch>
2016-02-03 12:58:47 +0100

    Change wallaby config for testing all src's

--- wallaby.conf.js
+++ wallaby.conf.js
@@ -12,8 +12,7 @@ module.exports = function (wallaby) {
   return {
     files: [
       {pattern: 'test/test-helper.js'},
-      {pattern: 'src/components/**/*.js'},
-      {pattern: 'src/*.js'}
+      {pattern: 'src/**/*.js'}
     ],
     tests: [
       {pattern: 'src/test/**/*.js'},
