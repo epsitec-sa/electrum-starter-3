3504ad3299d86f90df038c20e69db5f67780f17b
Samuel Loup <loup@epsitec.ch>
2016-02-05 14:01:57 +0100

    Refactor activity manager state

--- src/activities-manager.js
+++ src/activities-manager.js
@@ -1,15 +1,16 @@
 'use strict';
 import {State} from 'electrum-store';
 /******************************************************************************/
-const iniitalState = State.create ('am', {
+const initialState = State.create ('am', {
   currentActivity: null,
-  activities: {}
+  activities: {},
+  registry: {}
 });
 
 export default class ActivitiesManager {
   constructor (store) {
     this._store = store;
-    this._store.setState (iniitalState);
+    this._store.setState (initialState);
     this._generation = store.generation;
   }
 
@@ -25,6 +26,10 @@ export default class ActivitiesManager {
     return this.state.get ('activities');
   }
 
+  get registry () {
+    return this.state.get ('registry');
+  }
+
   get currentActivity () {
     return this.state.get ('currentActivity');
   }
@@ -36,15 +41,20 @@ export default class ActivitiesManager {
     this.state.set ('currentActivity', activityId);
   }
 
-  registerActivity (activity) {
-    if (this.activities[activity.id]) {
-      throw new Error (`Activity already registered: ${activity.id}`);
+  registerActivity (name, activityCreator) {
+    if (this.registry[name]) {
+      throw new Error (`Activity already registered: ${name}`);
     }
-    if (activity.id === 'am') {
-      throw new Error (`Activity id 'am' is reserved`);
-    }
+    this.registry[name] = activityCreator;
+    console.log (`Activity registered: ${name}`);
+  }
+
+  startActivity (name) {
+    const activity = this.registry[name] ();
     this.activities[activity.id] = activity.actuators;
     this.store.setState (activity.state);
+    this.currentActivity = activity.id;
+    console.log (`Activity ${name} started with id: ${activity.id}`);
   }
 
   doActionInActivity (action) {
@@ -53,7 +63,13 @@ export default class ActivitiesManager {
     }
     if (this.activities[this.currentActivity][action.type]) {
       console.log (`do ${action.type} in ${this.currentActivity}`);
-      this.activities[this.currentActivity][action.type] (this.store, action);
+      this.activities[this.currentActivity][action.type] (
+        this.store.select (this.currentActivity),
+        action,
+        this.store,
+        this.store,
+        this.doActionInActivity
+      );
     } else {
       console.log (`cannot do ${action.type} in ${this.currentActivity}`);
     }
@@ -67,6 +83,10 @@ export default class ActivitiesManager {
       {
       case 'SWITCH_ACTIVITY': {
         this.currentActivity = action.id;
+        break;
+      }
+      case 'START_ACTIVITY': {
+        this.startActivity (action.name);
         break;
       }
       default:
