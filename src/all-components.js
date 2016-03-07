5d713709b87831a99b67197e81793cb774eff73b
Daniel Roux <roux@epsitec.ch>
2016-03-07 17:01:05 +0100

    Ajout d'un composant Boolean.

--- src/all-components.js
+++ src/all-components.js
@@ -1,10 +1,12 @@
 'use strict';
 import Electrum from 'electrum';
+import _Boolean from './components/boolean.component.js';
 import _Button from './components/button.component.js';
 import _Button$styles from './components/button.styles.js';
 import _Collection from './components/collection.component.js';
 import _CurrentActivityInfo from './components/current-activity-info.component.js';
 import _Label from './components/label.component.js';
+export const Boolean = Electrum.wrap ('Boolean', _Boolean);
 export const Button = Electrum.wrap ('Button', _Button, {styles: _Button$styles});
 export const Collection = Electrum.wrap ('Collection', _Collection);
 export const CurrentActivityInfo = Electrum.wrap ('CurrentActivityInfo', _CurrentActivityInfo);
