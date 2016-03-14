d83f741ed39fc6478e7b2815ac8e339b1db255d7
Daniel Roux <roux@epsitec.ch>
2016-03-14 16:29:26 +0100

    Oubli.

--- src/components/badge.component.js
+++ src/components/badge.component.js
@@ -4,10 +4,8 @@ import React from 'react';
 
 export default class Badge extends React.Component {
   render () {
-    const {state, glyph} = this.props;
+    const {state} = this.props;
     const value = state.get ('value');
-
-
     return (
       <label style={this.styles}>
           {value}
