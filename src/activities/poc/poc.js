cabfa002902d0c3d5f98a620456c00b8c911c0b7
Daniel Roux <roux@epsitec.ch>
2016-03-17 09:22:26 +0100

    WIP: poc.js passe en flex, ce qui casse pas mal de choses...

--- src/activities/poc/poc.js
+++ src/activities/poc/poc.js
@@ -9,21 +9,36 @@ export default class Poc extends React.Component {
     const {state} = this.props;
     const name = state.get ('name');
     const Wizard = state.select ('wizard.[forms]').get ().view;
+
+    const mainStyle = {
+      display: 'flex',
+      flexWrap: 'wrap'
+    };
+
     return (
       <div>
-        {`Hello ${name}`}
-        <br/>
-        <Icon glyph="car" size="2x" {...this.link ('icon')}/>
-        <Badge {...this.link ('badge')} value="1"/>
-        <Badge {...this.link ('badge')} value="12"/>
-        <Badge {...this.link ('badge')} value="123"/>
-        <Badge {...this.link ('badge')}/>
-        <br/><br/>
-        <IconBadge glyph="car" size="1x" value="7" {...this.link ('icon-badge')}/>
-        <IconBadge glyph="car" size="2x" value="7" {...this.link ('icon-badge')}/>
-        <IconBadge glyph="car" size="3x" value="7" {...this.link ('icon-badge')}/>
-        <Button id={state.id} action={act.SAY_HELLO} {...this.link ()}>Say hello...</Button>
-        <Wizard {...this.link ('wizard.[forms]')} />
+        <div style={mainStyle}>
+          {`Hello ${name}`}
+        </div>
+        <div style={mainStyle}>
+          <Icon glyph="car" size="2x" {...this.link ('icon')}/>
+          <Badge {...this.link ('badge')} value="1"/>
+          <Badge {...this.link ('badge')} value="12"/>
+          <Badge {...this.link ('badge')} value="123"/>
+          <Badge {...this.link ('badge')}/>
+        </div>
+        <div style={mainStyle}>
+          <IconBadge glyph="car" size="1x" value="7" {...this.link ('icon-badge')}/>
+          <IconBadge glyph="car" size="2x" value="7" {...this.link ('icon-badge')}/>
+          <IconBadge glyph="car" size="3x" value="7" {...this.link ('icon-badge')}/>
+          <IconBadge glyph="car" size="2x" value="12" {...this.link ('icon-badge')}/>
+          <IconBadge glyph="car" size="2x" value="123" {...this.link ('icon-badge')}/>
+          <IconBadge glyph="car" size="2x" value="1234" {...this.link ('icon-badge')}/>
+        </div>
+        <div style={mainStyle}>
+          <Button id={state.id} action={act.SAY_HELLO} {...this.link ()}>Say hello...</Button>
+          <Wizard {...this.link ('wizard.[forms]')} />
+        </div>
       </div>
     );
   }
