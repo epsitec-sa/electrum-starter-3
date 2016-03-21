87573f644de1f9db450d5c6f105f081ad8f3d88e
Daniel Roux <roux@epsitec.ch>
2016-03-21 20:19:57 +0100

    Etendu le catalogue avec les nouveaux composants IconButton, RadioButton et Checkbox.

--- src/activities/catalog/catalog.js
+++ src/activities/catalog/catalog.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import React from 'react';
-import {FlatButton, RaisedButton, BasicList, ListItem} from 'electrum-arc';
+import {FlatButton, RaisedButton, IconButton, RadioButton, Checkbox, BasicList, ListItem} from 'electrum-arc';
 import act from './actuators.js';
 
 export default class Catalog extends React.Component {
@@ -19,6 +19,17 @@ export default class Catalog extends React.Component {
         <RaisedButton action={act.TEST} label="click me" id="totoflat" {...this.link ()} />
         <RaisedButton primary action={act.TEST} label="click me" id="totoflat" {...this.link ()} />
         <RaisedButton secondary action={act.TEST} label="click me" id="totoflat" {...this.link ()} />
+        <IconButton action={act.TEST} label="click me" id="totoflat" {...this.link ()} />
+        <hr />
+        <h1>Radio buttons</h1>
+        <RadioButton action={act.TEST} label="Rouge" checked id="radioRed" {...this.link ()} />
+        <RadioButton action={act.TEST} label="Vert" id="radioGreen" {...this.link ()} />
+        <RadioButton action={act.TEST} label="Bleu" id="radioBlue" {...this.link ()} />
+        <hr />
+        <h1>Checkbox buttons</h1>
+        <Checkbox action={act.TEST} label="Rouge" checked id="radioRed" {...this.link ()} />
+        <Checkbox action={act.TEST} label="Vert" id="radioGreen" {...this.link ()} />
+        <Checkbox action={act.TEST} label="Bleu" id="radioBlue" {...this.link ()} />
         <hr />
         <h1>Basic list exemple</h1>
         <BasicList {...this.link ('list')} template={listTemplate}>
