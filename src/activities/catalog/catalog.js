cc33ece129baa92e5d0a05f39f4ca3804896a888
Daniel Roux <roux@epsitec.ch>
2016-03-22 11:24:15 +0100

    Ajouté les nouveaux composants.

--- src/activities/catalog/catalog.js
+++ src/activities/catalog/catalog.js
@@ -1,7 +1,8 @@
 'use strict';
 
 import React from 'react';
-import {FlatButton, RaisedButton, IconButton, RadioButton, Checkbox, BasicList, ListItem} from 'electrum-arc';
+import {FlatButton, RaisedButton, IconButton, RadioButton, Checkbox, Badge,
+  Label, TextField, BasicField, BasicList, ListItem} from 'electrum-arc';
 import act from './actuators.js';
 
 export default class Catalog extends React.Component {
@@ -12,6 +13,8 @@ export default class Catalog extends React.Component {
         <ListItem key={state.key} primaryText={title} />
       );
     };
+    // <Label>Coucou</Label>
+    // <TextField hintText="Tralala" id="textField2" {...this.link ()} />
     return (
       <div>
         <h1>Catalog of buttons</h1>
@@ -20,6 +23,11 @@ export default class Catalog extends React.Component {
         <RaisedButton primary action={act.TEST} label="click me" id="totoflat" {...this.link ()} />
         <RaisedButton secondary action={act.TEST} label="click me" id="totoflat" {...this.link ()} />
         <IconButton action={act.TEST} label="click me" id="totoflat" {...this.link ()} />
+        <hr />
+        <div>
+          <RaisedButton primary action={act.TEST} label="Button with badge" id="totoflat" {...this.link ()} />
+          <Badge badgeContent="12" {...this.link ()} />
+        </div>
         <hr />
         <h1>Radio buttons</h1>
         <RadioButton action={act.TEST} label="Rouge" checked id="radioRed" {...this.link ()} />
@@ -30,6 +38,9 @@ export default class Catalog extends React.Component {
         <Checkbox action={act.TEST} label="Rouge" checked id="radioRed" {...this.link ()} />
         <Checkbox action={act.TEST} label="Vert" id="radioGreen" {...this.link ()} />
         <Checkbox action={act.TEST} label="Bleu" id="radioBlue" {...this.link ()} />
+        <hr />
+        <h1>Fields</h1>
+        <BasicField id="textField1" {...this.link ()} />
         <hr />
         <h1>Basic list exemple</h1>
         <BasicList {...this.link ('list')} template={listTemplate}>
