db24eeb691aeecc1ceea9d52ee629717994aa4d3
Daniel Roux <roux@epsitec.ch>
2016-04-15 16:34:37 +0200

    Add 'Prestation' and 'Destinataire' papiers.

--- src/activities/polypheme/polypheme.js
+++ src/activities/polypheme/polypheme.js
@@ -65,6 +65,7 @@ export default class Polypheme extends React.Component {
     return (
       <div style={{padding: '10px 0px 0px 30px'}}>
         <h1>Polyphème prototype</h1>
+
         <Paper zDepth={3} style={paperStyle3} {...this.link ()} >
           <BasicContainer width='600px' {...this.link ()} >
             <BasicRow>
@@ -92,6 +93,58 @@ export default class Polypheme extends React.Component {
             </BasicRow>
           </BasicContainer>
         </Paper>
+
+        <br />
+
+        <Paper zDepth={3} style={paperStyle3} {...this.link ()} >
+          <BasicContainer width='600px' {...this.link ()} >
+            <BasicRow>
+              <BasicLabel text="PRESTATION" grow="1" {...this.link ()} />
+              <RichButton glyph="ellipsis-v" {...this.link ()} />
+            </BasicRow>
+            <BasicRow>
+              <RichButton text="Standard (1h)" flowContinuation="spacing" {...this.link ()} />
+              <RichButton text="Urgent (30 min)" flowContinuation="spacing" {...this.link ()} />
+              <RichButton text="Dring Dring" {...this.link ()} />
+            </BasicRow>
+            <BasicRow>
+              <RichButton text="Course régulière" flowContinuation="spacing" {...this.link ()} />
+              <RichButton text="Multi" flowContinuation="spacing" {...this.link ()} />
+              <RichButton text="Case postale" {...this.link ()} />
+            </BasicRow>
+          </BasicContainer>
+        </Paper>
+
+        <br />
+
+        <Paper zDepth={3} style={paperStyle3} {...this.link ()} >
+          <BasicContainer width='600px' {...this.link ()} >
+            <BasicRow>
+              <BasicLabel text="DESTINATAIRE" grow="1" {...this.link ()} />
+              <RichButton glyph="toggle-off" text="Nouveau" borderless flowContinuation="spacing" {...this.link ()} />
+              <RichButton glyph="plus" text="Ajouter" flowContinuation="spacing" {...this.link ()} />
+              <RichButton glyph="ellipsis-v" {...this.link ()} />
+            </BasicRow>
+            <BasicRow>
+              <BasicField hintText="Date" grow="1" flowContinuation="overlay" {...this.link ()} />
+              <RichButton glyph="calendar" flowContinuation="spacing" {...this.link ()} />
+              <BasicField hintText="Période" grow="2" flowContinuation="overlay" {...this.link ()} />
+              <RichButton glyph="clock-o" flowContinuation="spacing" {...this.link ()} />
+              <BasicField hintText="Zone" grow="1" flowContinuation="overlay" {...this.link ()} />
+              <RichButton glyph="map-marker" {...this.link ()} />
+            </BasicRow>
+            <BasicRow>
+              <RichButton glyph="user" flowContinuation="overlay" {...this.link ()} />
+              <BasicField hintText="Nom du client" {...this.link ()} />
+            </BasicRow>
+            <BasicRow>
+              <RichButton glyph="comment" flowContinuation="overlay" {...this.link ()} />
+              <BasicField hintText="Remarque" flowContinuation="spacing" {...this.link ()} />
+              <RichButton glyph="square-o" text="facturé à" borderless rightIcon {...this.link ()} />
+            </BasicRow>
+          </BasicContainer>
+        </Paper>
+
         <br />
       </div>
     );
