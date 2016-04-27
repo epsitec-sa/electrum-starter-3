e86ebe464c81a19c2f38bcb57c275d99bb60ab1e
Daniel Roux <roux@epsitec.ch>
2016-04-27 10:44:03 +0200

    Add new TabContainer component.

--- src/activities/polypheme/polypheme.js
+++ src/activities/polypheme/polypheme.js
@@ -6,6 +6,7 @@ import {
   TextField,
   BasicRow,
   BasicContainer,
+  TabContainer,
   Label
 } from 'electrum-arc';
 
@@ -34,13 +35,13 @@ export default class Polypheme extends React.Component {
         <h1>Polyphème prototype</h1>
 
         <BasicContainer border="none" kind="root" {...this.link ()} >
-          <BasicRow>
+          <TabContainer>
             <RichButton text="Expéditeur"   width="0px" grow="1" active="true"  kind="tab" id="exp.Tab" {...this.link ()} />
             <RichButton text="Prestation"   width="0px" grow="1" active="false" kind="tab" id="exp.Tab" {...this.link ()} />
             <RichButton text="Destinataire" width="0px" grow="1" active="false" kind="tab" id="exp.Tab" {...this.link ()} />
             <RichButton text="Colis"        width="0px" grow="1" active="false" kind="tab" id="exp.Tab" {...this.link ()} />
             <RichButton text="Facture"      width="0px" grow="1" active="false" kind="tab" id="exp.Tab" {...this.link ()} />
-          </BasicRow>
+          </TabContainer>
 
           <BasicContainer width='600px' zDepth={3} spacing="large" border="none" {...this.link ()} >
             <BasicRow>
