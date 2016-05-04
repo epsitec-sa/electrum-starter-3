adf430f475d153c56c283e9bbdc8b705f74bad98
Daniel Roux <roux@epsitec.ch>
2016-05-04 17:49:25 +0200

    Replace BasicRowContainer by RichContainer.

--- src/activities/polypheme/polypheme.js
+++ src/activities/polypheme/polypheme.js
@@ -5,7 +5,6 @@ import {
   RichContainer,
   RichButton,
   TextField,
-  BasicRow,
   Label
 } from 'electrum-arc';
 
@@ -65,81 +64,81 @@ export default class Polypheme extends React.Component {
 
             <RichContainer kind='panes' {...this.link ()} >
               <RichContainer kind='pane' width='600px' {...this.link ()} >
-                <BasicRow {...this.link ()} >
+                <RichContainer kind='rowPane' {...this.link ()} >
                   <Label text="Expéditeur" grow="1" kind="title" {...this.link ()} />
                   <RichButton glyph="toggle-off" text="Nouveau" border="none" spacing="large" id="exp.newButton" {...this.link ()} />
                   <RichButton glyph="plus" text="Ajouter" spacing="large" id="exp.addButton" {...this.link ()} />
                   <RichButton glyph="ellipsis-v" id="exp.extButton" {...this.link ()} />
-                </BasicRow>
-                <BasicRow {...this.link ()} >
+                </RichContainer>
+                <RichContainer kind='rowPane' {...this.link ()} >
                   <TextField hintText="Date" grow="1" spacing="overlap" id="exp.date" {...this.link ()} />
                   <RichButton glyph="calendar" spacing="large" {...this.link ()} />
                   <TextField hintText="Période" grow="2" spacing="overlap" id="exp.period" {...this.link ()} />
                   <RichButton glyph="clock-o" spacing="large" {...this.link ()} />
                   <TextField hintText="Zone" grow="1" spacing="overlap" id="exp.zone" {...this.link ()} />
                   <RichButton glyph="map-marker" {...this.link ()} />
-                </BasicRow>
-                <BasicRow {...this.link ()} >
+                </RichContainer>
+                <RichContainer kind='rowPane' {...this.link ()} >
                   <RichButton glyph="user" spacing="overlap" {...this.link ()} />
                   <TextField hintText="Nom du client" id="exp.customer" {...this.link ()} />
-                </BasicRow>
-                <BasicRow {...this.link ()} >
+                </RichContainer>
+                <RichContainer kind='rowPane' {...this.link ()} >
                   <RichButton glyph="tag" spacing="overlap" {...this.link ()} />
                   <TextField hintText="Référence client" spacing="large" id="exp.ref" {...this.link ()} />
                   <RichButton glyph="check-square" text="facturé à" border="none" glyphPosition="right" {...this.link ()} />
-                </BasicRow>
+                </RichContainer>
               </RichContainer>
 
               <RichContainer kind='pane' width='600px' {...this.link ()} >
-                <BasicRow {...this.link ()} >
+                <RichContainer kind='rowPane' {...this.link ()} >
                   <Label text="Prestation" grow="1" kind="title" {...this.link ()} />
                   <RichButton glyph="ellipsis-v" id="custom.miscButton" {...this.link ()} />
-                </BasicRow>
-                <BasicRow {...this.link ()} >
+                </RichContainer>
+                <RichContainer kind='rowPane' {...this.link ()} >
                   <RichButton text="Standard (1h)"    width="0px" grow="1" spacing="large" id="custom.1" {...this.link ()} />
                   <RichButton text="Urgent (30 min)"  width="0px" grow="1" spacing="large" id="custom.2" {...this.link ()} />
                   <RichButton text="Dring Dring"      width="0px" grow="1" id="custom.3" {...this.link ()} />
-                </BasicRow>
-                <BasicRow {...this.link ()} >
+                </RichContainer>
+                <RichContainer kind='rowPane' {...this.link ()} >
                   <RichButton text="Course régulière" width="0px" grow="1" spacing="large" id="custom.4" {...this.link ()} />
                   <RichButton text="Multi"            width="0px" grow="1" spacing="large" id="custom.5" {...this.link ()} />
                   <RichButton text="Case postale"     width="0px" grow="1" id="custom.6" {...this.link ()} />
-                </BasicRow>
+                </RichContainer>
               </RichContainer>
 
               <RichContainer kind='pane' width='600px' {...this.link ()} >
-                <BasicRow {...this.link ()} >
+                <RichContainer kind='rowPane' {...this.link ()} >
                   <Label text="Destinataire" grow="1" kind="title" {...this.link ()} />
                   <RichButton glyph="toggle-off" text="Nouveau" border="none" spacing="large" id="dest.newButton" {...this.link ()} />
                   <RichButton glyph="plus" text="Ajouter" spacing="large" id="dest.addButton" {...this.link ()} />
                   <RichButton glyph="ellipsis-v" id="dest.miscButton" {...this.link ()} />
-                </BasicRow>
-                <BasicRow {...this.link ()} >
+                </RichContainer>
+                <RichContainer kind='rowPane' {...this.link ()} >
                   <TextField hintText="Date" grow="1" spacing="overlap" id="dest.date" {...this.link ()} />
                   <RichButton glyph="calendar" spacing="large" {...this.link ()} />
                   <TextField hintText="Période" grow="2" spacing="overlap" id="dest.period" {...this.link ()} />
                   <RichButton glyph="clock-o" spacing="large" {...this.link ()} />
                   <TextField hintText="Zone" grow="1" spacing="overlap" id="dest.zone" {...this.link ()} />
                   <RichButton glyph="map-marker" {...this.link ()} />
-                </BasicRow>
-                <BasicRow {...this.link ()} >
+                </RichContainer>
+                <RichContainer kind='rowPane' {...this.link ()} >
                   <RichButton glyph="user" spacing="overlap" {...this.link ()} />
                   <TextField hintText="Nom du client" id="dest.customer" {...this.link ()} />
-                </BasicRow>
-                <BasicRow {...this.link ()} >
+                </RichContainer>
+                <RichContainer kind='rowPane' {...this.link ()} >
                   <RichButton glyph="comment" spacing="overlap" {...this.link ()} />
                   <TextField hintText="Remarque" spacing="large" id="dest.comment" {...this.link ()} />
                   <RichButton glyph="square-o" text="facturé à" border="none" glyphPosition="right" {...this.link ()} />
-                </BasicRow>
+                </RichContainer>
               </RichContainer>
 
               <RichContainer kind='pane' width='600px' {...this.link ()} >
-                <BasicRow {...this.link ()} >
+                <RichContainer kind='rowPane' {...this.link ()} >
                   <Label text="Colis" grow="1" kind="title" {...this.link ()} />
                   <RichButton glyph="plus" text="Ajouter" spacing="large" id="package.addButton" {...this.link ()} />
                   <RichButton glyph="ellipsis-v" id="package.miscButton" {...this.link ()} />
-                </BasicRow>
-                <BasicRow {...this.link ()} >
+                </RichContainer>
+                <RichContainer kind='rowPane' {...this.link ()} >
                   <RichButton glyph="trash-o" spacing="large" id="package.delete.1" {...this.link ()} />
                   <TextField value="Standard box" hintText="Type" grow="1" spacing="overlap" id="package.box.1" {...this.link ()} />
                   <RichButton glyph="cube" spacing="large" {...this.link ()} />
@@ -147,8 +146,8 @@ export default class Polypheme extends React.Component {
                   <RichButton glyph="balance-scale" spacing="large" {...this.link ()} />
                   <TextField value="50 x 75 x 20" hintText="Dimensions" grow="1" spacing="overlap" id="package.dim.1" {...this.link ()} />
                   <RichButton glyph="arrows-h" {...this.link ()} />
-                </BasicRow>
-                <BasicRow {...this.link ()} >
+                </RichContainer>
+                <RichContainer kind='rowPane' {...this.link ()} >
                   <RichButton glyph="trash-o" spacing="large" id="package.delete.2" {...this.link ()} />
                   <TextField hintText="Type" grow="1" spacing="overlap" id="package.box.2" {...this.link ()} />
                   <RichButton glyph="cube" spacing="large" {...this.link ()} />
@@ -156,44 +155,44 @@ export default class Polypheme extends React.Component {
                   <RichButton glyph="balance-scale" spacing="large" {...this.link ()} />
                   <TextField hintText="Dimensions" grow="1" spacing="overlap" id="package.dim.2" {...this.link ()} />
                   <RichButton glyph="arrows-h" {...this.link ()} />
-                </BasicRow>
-                <BasicRow {...this.link ()} >
+                </RichContainer>
+                <RichContainer kind='rowPane' {...this.link ()} >
                   <Label text="Nb de colis 2"     kind="info" width="0px" grow="1" id="package.infoCount" {...this.link ()} />
                   <Label text="Total 3.6 kg"      kind="info" width="0px" grow="1" id="package.infoWeight" {...this.link ()} />
                   <Label text="IATA total 3.6 kg" kind="info" width="0px" grow="1" id="package.infoTotal" {...this.link ()} />
-                </BasicRow>
-                <BasicRow {...this.link ()} >
+                </RichContainer>
+                <RichContainer kind='rowPane' {...this.link ()} >
                   <RichButton glyph="comment" spacing="overlap" {...this.link ()} />
                   <TextField hintText="Remarque" id="package.comment" {...this.link ()} />
-                </BasicRow>
+                </RichContainer>
               </RichContainer>
 
               <RichContainer kind='pane' width='600px' {...this.link ()} >
-                <BasicRow {...this.link ()} >
+                <RichContainer kind='rowPane' {...this.link ()} >
                   <Label text="Facture" grow="1" kind="title" {...this.link ()} />
                   <RichButton glyph="plus" text="Ajouter" spacing="large" id="fact.addButton" {...this.link ()} />
                   <RichButton glyph="ellipsis-v" id="fact.extButton" {...this.link ()} />
-                </BasicRow>
-                <BasicRow spacing="compact">
+                </RichContainer>
+                <RichContainer kind='rowPane' spacing="compact" {...this.link ()} >
                   <Label text="Standard A - C"   kind="article" width="0px" grow="4" {...this.link ()} />
                   <Label text="50.00"            kind="price"   width="0px" grow="1" {...this.link ()} />
-                </BasicRow>
-                <BasicRow spacing="compact">
+                </RichContainer>
+                <RichContainer kind='rowPane' spacing="compact" {...this.link ()} >
                   <Label text="Supplément poids" kind="article" width="0px" grow="4" {...this.link ()} />
                   <Label text="12.00"            kind="price"   width="0px" grow="1" {...this.link ()} />
-                </BasicRow>
-                <BasicRow spacing="compact">
+                </RichContainer>
+                <RichContainer kind='rowPane' spacing="compact" {...this.link ()} >
                   <Label text="Sous-total"       kind="articleFooter" width="0px" grow="4" {...this.link ()} />
                   <Label text="62.00"            kind="priceFooter"   width="0px" grow="1" {...this.link ()} />
-                </BasicRow>
-                <BasicRow spacing="compact">
+                </RichContainer>
+                <RichContainer kind='rowPane' spacing="compact" {...this.link ()} >
                   <Label text="Avance d'argent"  kind="article" width="0px" grow="4" {...this.link ()} />
                   <Label text="55.00"            kind="price"   width="0px" grow="1" {...this.link ()} />
-                </BasicRow>
-                <BasicRow {...this.link ()} >
+                </RichContainer>
+                <RichContainer kind='rowPane' {...this.link ()} >
                   <Label text="Total"            kind="articleFooter" width="0px" grow="4" {...this.link ()} />
                   <Label text="117.00"           kind="priceFooter"   width="0px" grow="1" {...this.link ()} />
-                </BasicRow>
+                </RichContainer>
               </RichContainer>
             </RichContainer>
 
