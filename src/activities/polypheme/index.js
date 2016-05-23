d97817a28ecdef84053ac66a32a58e5a13ee3bb9
Samuel Loup <samuel.loup@gmail.com>
2016-05-23 15:38:48 +0200

    Add exemple for wiring notifiers (onInit activity)

--- src/activities/polypheme/index.js
+++ src/activities/polypheme/index.js
@@ -5,6 +5,23 @@ import actuators from './actuators.js';
 import Activity from '../activity.js';
 
 const onInit = (state) => {
+  const expDateModifier = state.select ('exp-date');
+  expDateModifier.set ('onChange', (value, state) => {
+    console.log (`CHANGE ${value}`);
+    console.dir (state);
+    state
+      .set ('toto', `TOTO:${value}`)
+      .set ('combo-glyph', value);
+  }).set ('onFocus', (value, state) => {
+    console.log (`ONFOCUS ${value}`);
+    state.set ('combo-glyph', 'car');
+    console.dir (state);
+  }).set ('onDefocus', (value, state) => {
+    console.log (`ONDEFOCUS ${value}`);
+    state.set ('combo-glyph', '');
+    console.dir (state);
+  });
+
   const field = state.select ('field');
   field
     .set ('value', '')
