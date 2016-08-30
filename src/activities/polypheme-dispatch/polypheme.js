0256f813a638d1bde7af4bc35558dcef151ab4b7
Daniel Roux <roux@epsitec.ch>
2016-08-30 14:20:24 +0200

    Some layout adjustements.

--- src/activities/polypheme-dispatch/polypheme.js
+++ src/activities/polypheme-dispatch/polypheme.js
@@ -100,7 +100,7 @@ export default class Polypheme extends React.Component {
 
             <Container kind='view' {...this.link ()} >
               <Container kind='tickets' {...this.link ()} >
-                <Ticket kind='header' width='300px' height='70px' {...this.link ()} >
+                <Ticket kind='header' width='300px' height='90px' {...this.link ()} >
                   <Container kind='column' grow='2' {...this.link ()} >
                     <Button glyph='user' kind='identity' {...this.link ()} />
                   </Container>
@@ -112,7 +112,7 @@ export default class Polypheme extends React.Component {
                     <Label text='203.50' font-weight='bold' text-color='#fff' {...this.link ()} />
                   </Container>
                 </Ticket>
-                <Ticket width='300px' height='70px' {...this.link ()} >
+                <Ticket width='300px' height='90px' {...this.link ()} >
                   <Container kind='column' grow='1' {...this.link ()} >
                     <Label text='10:50' font-weight='bold' {...this.link ()} />
                     <Label glyph='upload' {...this.link ()} />
@@ -125,7 +125,7 @@ export default class Polypheme extends React.Component {
                     </Container>
                   </Container>
                 </Ticket>
-                <Ticket width='300px' height='70px' {...this.link ()} >
+                <Ticket width='300px' height='90px' {...this.link ()} >
                   <Container kind='column' grow='1' {...this.link ()} >
                     <Label text='11:00' font-weight='bold' {...this.link ()} />
                     <Label glyph='upload' {...this.link ()} />
@@ -137,7 +137,7 @@ export default class Polypheme extends React.Component {
                     </Container>
                   </Container>
                 </Ticket>
-                <Ticket width='300px' height='70px' {...this.link ()} >
+                <Ticket width='300px' height='90px' {...this.link ()} >
                   <Container kind='column' grow='1' {...this.link ()} >
                     <Label text='11:35' font-weight='bold' {...this.link ()} />
                     <Label glyph='download' {...this.link ()} />
@@ -150,7 +150,7 @@ export default class Polypheme extends React.Component {
                     </Container>
                   </Container>
                 </Ticket>
-                <Ticket width='300px' height='70px' {...this.link ()} >
+                <Ticket width='300px' height='90px' {...this.link ()} >
                   <Container kind='column' grow='1' {...this.link ()} >
                     <Label text='11:45' font-weight='bold' {...this.link ()} />
                     <Label glyph='download' {...this.link ()} />
@@ -164,7 +164,7 @@ export default class Polypheme extends React.Component {
                     </Container>
                   </Container>
                 </Ticket>
-                <Ticket width='300px' height='70px' {...this.link ()} >
+                <Ticket width='300px' height='90px' {...this.link ()} >
                   <Container kind='column' grow='1' {...this.link ()} >
                     <Label text='13:15' font-weight='bold' {...this.link ()} />
                     <Label glyph='upload' {...this.link ()} />
@@ -181,7 +181,7 @@ export default class Polypheme extends React.Component {
 
             <Container kind='view' {...this.link ()} >
               <Container kind='tickets' {...this.link ()} >
-                <Ticket kind='header' width='300px' height='70px' {...this.link ()} >
+                <Ticket kind='header' width='300px' height='90px' {...this.link ()} >
                   <Container kind='column' grow='2' {...this.link ()} >
                     <Button glyph='user' kind='identity' {...this.link ()} />
                   </Container>
@@ -193,7 +193,7 @@ export default class Polypheme extends React.Component {
                     <Label text='68.00' font-weight='bold' text-color='#fff' {...this.link ()} />
                   </Container>
                 </Ticket>
-                <Ticket width='300px' height='70px' {...this.link ()} >
+                <Ticket width='300px' height='90px' {...this.link ()} >
                   <Container kind='column' grow='1' {...this.link ()} >
                     <Label text='10:10' font-weight='bold' {...this.link ()} />
                     <Label glyph='upload' {...this.link ()} />
@@ -206,7 +206,7 @@ export default class Polypheme extends React.Component {
                     </Container>
                   </Container>
                 </Ticket>
-                <Ticket width='300px' height='70px' {...this.link ()} >
+                <Ticket width='300px' height='90px' {...this.link ()} >
                   <Container kind='column' grow='1' {...this.link ()} >
                     <Label text='10:30' font-weight='bold' {...this.link ()} />
                     <Label glyph='download' {...this.link ()} />
@@ -223,7 +223,7 @@ export default class Polypheme extends React.Component {
 
             <Container kind='view' {...this.link ()} >
               <Container kind='tickets' {...this.link ()} >
-                <Ticket kind='header' width='300px' height='70px' {...this.link ()} >
+                <Ticket kind='header' width='300px' height='90px' {...this.link ()} >
                   <Container kind='column' grow='2' {...this.link ()} >
                     <Button glyph='user-secret' kind='identity' {...this.link ()} />
                   </Container>
@@ -235,32 +235,7 @@ export default class Polypheme extends React.Component {
                     <Label text='250.00' font-weight='bold' text-color='#fff' {...this.link ()} />
                   </Container>
                 </Ticket>
-                <Ticket width='300px' height='70px' {...this.link ()} >
-                  <Container kind='column' grow='1' {...this.link ()} >
-                    <Label text='10:50' font-weight='bold' {...this.link ()} />
-                    <Label glyph='upload' {...this.link ()} />
-                  </Container>
-                  <Container kind='column' grow='3' {...this.link ()} >
-                    <Label text='Coop St. Laurent' {...this.link ()} />
-                    <Container kind='row' {...this.link ()} >
-                      <Label glyph='cube' text='1x' width='150px' {...this.link ()} />
-                    </Container>
-                  </Container>
-                </Ticket>
-                <Ticket width='300px' height='70px' {...this.link ()} >
-                  <Container kind='column' grow='1' {...this.link ()} >
-                    <Label text='11:00' font-weight='bold' {...this.link ()} />
-                    <Label glyph='upload' {...this.link ()} />
-                  </Container>
-                  <Container kind='column' grow='3' {...this.link ()} >
-                    <Label text='Migros Pont-Neuf' {...this.link ()} />
-                    <Container kind='row' {...this.link ()} >
-                      <Label glyph='cube' text='3x' width='150px' {...this.link ()} />
-                      <Label glyph='warning' {...this.link ()} />
-                    </Container>
-                  </Container>
-                </Ticket>
-                <Ticket width='300px' height='70px' {...this.link ()} >
+                <Ticket width='300px' height='90px' {...this.link ()} >
                   <Container kind='column' grow='1' {...this.link ()} >
                     <Label text='11:35' font-weight='bold' {...this.link ()} />
                     <Label glyph='download' {...this.link ()} />
@@ -272,7 +247,7 @@ export default class Polypheme extends React.Component {
                     </Container>
                   </Container>
                 </Ticket>
-                <Ticket width='300px' height='70px' {...this.link ()} >
+                <Ticket width='300px' height='90px' {...this.link ()} >
                   <Container kind='column' grow='1' {...this.link ()} >
                     <Label text='11:45' font-weight='bold' {...this.link ()} />
                     <Label glyph='download' {...this.link ()} />
@@ -284,7 +259,7 @@ export default class Polypheme extends React.Component {
                     </Container>
                   </Container>
                 </Ticket>
-                <Ticket width='300px' height='70px' {...this.link ()} >
+                <Ticket width='300px' height='90px' {...this.link ()} >
                   <Container kind='column' grow='1' {...this.link ()} >
                     <Label text='13:15' font-weight='bold' {...this.link ()} />
                     <Label glyph='upload' {...this.link ()} />
@@ -297,7 +272,7 @@ export default class Polypheme extends React.Component {
                     </Container>
                   </Container>
                 </Ticket>
-                <Ticket width='300px' height='70px' {...this.link ()} >
+                <Ticket width='300px' height='90px' {...this.link ()} >
                   <Container kind='column' grow='1' {...this.link ()} >
                     <Label text='14:20' font-weight='bold' {...this.link ()} />
                     <Label glyph='upload' {...this.link ()} />
@@ -310,7 +285,7 @@ export default class Polypheme extends React.Component {
                     </Container>
                   </Container>
                 </Ticket>
-                <Ticket width='300px' height='70px' {...this.link ()} >
+                <Ticket width='300px' height='90px' {...this.link ()} >
                   <Container kind='column' grow='1' {...this.link ()} >
                     <Label text='14:50' font-weight='bold' {...this.link ()} />
                     <Label glyph='upload' {...this.link ()} />
@@ -322,7 +297,7 @@ export default class Polypheme extends React.Component {
                     </Container>
                   </Container>
                 </Ticket>
-                <Ticket width='300px' height='70px' {...this.link ()} >
+                <Ticket width='300px' height='90px' {...this.link ()} >
                   <Container kind='column' grow='1' {...this.link ()} >
                     <Label text='16:00' font-weight='bold' {...this.link ()} />
                     <Label glyph='download' {...this.link ()} />
@@ -335,7 +310,7 @@ export default class Polypheme extends React.Component {
                     </Container>
                   </Container>
                 </Ticket>
-                <Ticket width='300px' height='70px' {...this.link ()} >
+                <Ticket width='300px' height='90px' {...this.link ()} >
                   <Container kind='column' grow='1' {...this.link ()} >
                     <Label text='16:10' font-weight='bold' {...this.link ()} />
                     <Label glyph='download' {...this.link ()} />
@@ -349,7 +324,7 @@ export default class Polypheme extends React.Component {
                     </Container>
                   </Container>
                 </Ticket>
-                <Ticket width='300px' height='70px' {...this.link ()} >
+                <Ticket width='300px' height='90px' {...this.link ()} >
                   <Container kind='column' grow='1' {...this.link ()} >
                     <Label text='16:45' font-weight='bold' {...this.link ()} />
                     <Label glyph='upload' {...this.link ()} />
