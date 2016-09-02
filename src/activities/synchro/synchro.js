25fe27d0330ac2273f1b9a6e70eda6c25fb8bdbc
Daniel Roux <roux@epsitec.ch>
2016-09-02 15:33:29 +0200

    Populate the view of Synchro.

--- src/activities/synchro/synchro.js
+++ src/activities/synchro/synchro.js
@@ -136,6 +136,73 @@ export default class Synchro extends React.Component {
           {this.viewTab ()}
 
           <Container kind='views' {...this.link ()} >
+            <Container kind='view' width='700px' {...this.link ()} >
+
+              <Container kind='pane-navigator' {...this.link ()} >
+                <Button text='Machine CGU' width='0px' grow='1' active='true'  kind='pane-navigator' {...this.link ()} />
+                <Button text='Machine Dupond' width='0px' grow='1' active='false'  kind='pane-navigator' {...this.link ()} />
+                <Button text='Utilisateurs' width='0px' grow='1' active='false' kind='pane-navigator' {...this.link ()} />
+              </Container>
+
+              <Container kind='panes' {...this.link ()} >
+
+                <Container kind='pane' {...this.link ()} >
+                  <Container kind='row-pane' {...this.link ()} >
+                    <Label text='Machine CGU' grow='1' kind='title' {...this.link ()} />
+                  </Container>
+                  <Container kind='row-pane' {...this.link ()} >
+                    <Label glyph='folder' text='Mimi.cre' grow='1' {...this.link ()} />
+                    <Label text='01.01.2016 - 31.12.2016' grow='1' {...this.link ()} />
+                    <Button glyph='ellipsis-v' border='none' {...this.link ()} />
+                  </Container>
+                  <Container kind='row-pane' {...this.link ()} >
+                    <Label glyph='folder' text='MimiSal_2016.salx' grow='1' {...this.link ()} />
+                    <Label text='01.01.2016 - 31.12.2016' grow='1' {...this.link ()} />
+                    <Button glyph='ellipsis-v' border='none' {...this.link ()} />
+                  </Container>
+                </Container>
+
+                <Container kind='pane' {...this.link ()} >
+                  <Container kind='row-pane' {...this.link ()} >
+                    <Label text='Machine Dupond' grow='1' kind='title' {...this.link ()} />
+                  </Container>
+                  <Container kind='row-pane' {...this.link ()} >
+                    <Label glyph='folder' text='Fidu.cre' grow='1' {...this.link ()} />
+                    <Label text='01.01.2016 - 31.12.2016' grow='1' {...this.link ()} />
+                    <Button glyph='ellipsis-v' border='none' {...this.link ()} />
+                  </Container>
+                  <Container kind='row-pane' {...this.link ()} >
+                    <Label glyph='folder' text='Fidu_2016.salx' grow='1' {...this.link ()} />
+                    <Label text='01.01.2016 - 31.12.2016' grow='1' {...this.link ()} />
+                    <Button glyph='ellipsis-v' border='none' {...this.link ()} />
+                  </Container>
+                  <Container kind='row-pane' {...this.link ()} >
+                    <Label glyph='folder' text='Fidu.fac' grow='1' {...this.link ()} />
+                    <Label text='01.01.2016 - 31.12.2016' grow='1' {...this.link ()} />
+                    <Button glyph='ellipsis-v' border='none' {...this.link ()} />
+                  </Container>
+                </Container>
+
+                <Container kind='pane' {...this.link ()} >
+                  <Container kind='row-pane' {...this.link ()} >
+                    <Label text='Utilisateurs' grow='1' kind='title' {...this.link ()} />
+                  </Container>
+                  <Container kind='row-pane' {...this.link ()} >
+                    <Label glyph='user' text='guidi@epsitec.ch' grow='1' {...this.link ()} />
+                  </Container>
+                  <Container kind='row-pane' {...this.link ()} >
+                    <Label glyph='user' text='jean.dupond@bluewin.ch' grow='1' {...this.link ()} />
+                  </Container>
+                </Container>
+
+                </Container>
+
+                <Container kind='actions' {...this.link ()} >
+                  <Button glyph='link' text='Attacher' width='0px' grow='1' kind='action' place='left' {...this.link ()} />
+                  <Button glyph='save' text='Enregistrer ticket' width='0px' grow='1' kind='action' place='right' {...this.link ()} />
+                </Container>
+
+            </Container>
           </Container>
 
           <Container kind='footer' {...this.link ()} >
