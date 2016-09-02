023f5e86044083cc22be08dfb02f15265cef7355
Daniel Roux <roux@epsitec.ch>
2016-09-02 11:45:51 +0200

    Added new TabButton and Button with kind='task-tab', for Synchro.

--- src/activities/synchro/synchro.js
+++ src/activities/synchro/synchro.js
@@ -41,12 +41,29 @@ export default class Synchro extends React.Component {
     return (
       <Container kind='root' {...this.link ()} >
 
-        <Container kind='task' {...this.link ()} >
+        <Container kind='task' width='300px' {...this.link ()} >
+          <Button action={act.NEXT ()} glyph='cloud' text='Crésus Synchro'
+            kind='task-logo' {...this.link ()} />
+          <Button glyph='refresh' text='Gestion des mandats'
+            kind='task' {...this.link ()} />
+          <TabButton text='Mon beau bouquet' kind='task' active='true' {...this.link ()} />
+          <TabButton text='Chorale Fa-si-la chanter' kind='task' active='false' {...this.link ()} />
+          <TabButton text='Club VTT' kind='task' active='false' {...this.link ()} />
         </Container>
 
         <Container kind='right' {...this.link ()} >
 
+          <Container kind='main-tab' {...this.link ()} >
+          </Container>
+
+          <Container kind='views' {...this.link ()} >
+          </Container>
+
+          <Container kind='footer' {...this.link ()} >
+          </Container>
+
         </Container>
+
       </Container>
     );
   }
