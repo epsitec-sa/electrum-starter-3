43605bbcae2553dd3bdf78cf1a1799136c231631
Daniel Roux <roux@epsitec.ch>
2016-09-02 12:35:24 +0200

    Improve the task container for Synchro.

--- src/activities/synchro/synchro.js
+++ src/activities/synchro/synchro.js
@@ -44,8 +44,11 @@ export default class Synchro extends React.Component {
         <Container kind='task' width='300px' {...this.link ()} >
           <Button action={act.NEXT ()} glyph='cloud' text='Crésus Synchro'
             kind='task-logo' {...this.link ()} />
-          <Button glyph='refresh' text='Gestion des mandats'
-            kind='task' {...this.link ()} />
+          <Label text='Gestion des mandats' kind='task' {...this.link ()} />
+          <Button text='Mettre à jour' glyph='refresh' tooltip='Met à jour la liste des mandats' kind='task-tab' {...this.link ()} />
+          <Button text='Nouveau mandat' glyph='plus' tooltip='Crée un nouveau mandat' kind='task-tab' {...this.link ()} />
+          <Button text='Utiliser un ticket' glyph='file' tooltip='Utilise un ticket pour se connecter à un autre mandat' kind='task-tab' {...this.link ()} />
+          <Label text='Vos mandats' kind='task' {...this.link ()} />
           <TabButton text='Mon beau bouquet' kind='task' active='true' {...this.link ()} />
           <TabButton text='Chorale Fa-si-la chanter' kind='task' active='false' {...this.link ()} />
           <TabButton text='Club VTT' kind='task' active='false' {...this.link ()} />
