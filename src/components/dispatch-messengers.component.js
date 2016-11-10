18bac994235f080638368f15832c7c726321885a
Daniel Roux <roux@epsitec.ch>
2016-11-10 14:08:33 +0100

    Improved the layout of TripBox when width is changing.

--- src/components/dispatch-messengers.component.js
+++ src/components/dispatch-messengers.component.js
@@ -663,7 +663,7 @@ export default class DispatchMessengers extends React.Component {
             </Container>
 
           </Container>
-          <Splitter kind='vertical' default-size='700px' max-size='700px' {...this.link ()} >
+          <Splitter kind='vertical' default-size='750px' {...this.link ()} >
             <Container kind='view' grow='1' {...this.link ()} >
               <Container kind='pane-top' {...this.link ()} >
                 <TextFieldCombo hint-text='Date' combo-glyph='calendar'
