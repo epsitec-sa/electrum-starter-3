1668bd2d417433c0398081ebe9726aac9ead3705
Daniel Roux <roux@epsitec.ch>
2017-02-07 18:15:22 +0100

    WIP: Next step into Events component.

--- src/activities/all-in-one/data-events.js
+++ src/activities/all-in-one/data-events.js
@@ -2,13 +2,13 @@
 
 const data =
 {
-  FromDate: '2017-02-01',
-  ToDate: '2017-02-28',
+  FromDate: '2017-02-06',
+  ToDate: '2017-02-12',
   Events: [
     {
-      FromDate: '2017-02-06',
+      FromDate: '2017-02-07',
       FromTime: '14:00:00',
-      ToDate: '2017-02-06',
+      ToDate: '2017-02-07',
       ToTime: '14:30:00',
       Note:
       {
@@ -17,9 +17,9 @@ const data =
       }
     },
     {
-      FromDate: '2017-02-06',
+      FromDate: '2017-02-07',
       FromTime: '15:10:00',
-      ToDate: '2017-02-06',
+      ToDate: '2017-02-07',
       ToTime: '15:45:00',
       Note:
       {
@@ -28,14 +28,57 @@ const data =
       }
     },
     {
-      FromDate: '2017-02-07',
+      FromDate: '2017-02-08',
       FromTime: '09:30:00',
-      ToDate: '2017-02-07',
+      ToDate: '2017-02-08',
       ToTime: '11:30:00',
       Note:
       {
-        Content: 'Ceci est une texte beaucoup plus long',
+        Content: 'Ceci est un texte beaucoup plus long',
         Glyphs: [{Glyph: 'bicycle'}],
+      }
+    },
+    {
+      FromDate: '2017-02-08',
+      FromTime: '11:05:00',
+      ToDate: '2017-02-08',
+      ToTime: '11:25:00',
+      Note:
+      {
+        Content: 'Bonjour',
+      }
+    },
+    {
+      FromDate: '2017-02-08',
+      FromTime: '14:20:00',
+      ToDate: '2017-02-08',
+      ToTime: '15:00:00',
+      Note:
+      {
+        Content: 'Rendre le rapport',
+        Glyphs: [{Glyph: 'bell'}],
+      }
+    },
+    {
+      FromDate: '2017-02-08',
+      FromTime: '14:45:00',
+      ToDate: '2017-02-08',
+      ToTime: '15:10:00',
+      Note:
+      {
+        Content: 'A',
+        Glyphs: [{Glyph: 'truck'}],
+      }
+    },
+    {
+      FromDate: '2017-02-10',
+      FromTime: '08:00:00',
+      ToDate: '2017-02-10',
+      ToTime: '17:00:00',
+      Note:
+      {
+        Content: 'Suite',
+        Glyphs: [{Glyph: 'user'}],
       }
     },
   ],
